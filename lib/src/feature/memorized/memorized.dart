43adb8b82b88d03d98e4da26db5af0e3cd638f80
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-19 20:59:06 +0900

    refactor: コミット漏れ

--- lib/src/feature/memorized/memorized.dart
+++ lib/src/feature/memorized/memorized.dart
@@ -92,39 +92,4 @@ class _MemorizedState extends State<Memorized> {
   }
 }
 
-class MemorizedMenuButtons extends StatelessWidget {
-  const MemorizedMenuButtons({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const Text(
-          'useMemoized サンプル',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 8),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const Memorized()),
-            );
-          },
-          child: const Text('StatefulWidget版'),
-        ),
-        const SizedBox(height: 8),
-        ElevatedButton(
-          onPressed: () {
-            // Navigator.push(
-            //   context,
-            //   // MaterialPageRoute(builder: (_) => const UserSearchHooks()),
-            // );
-          },
-          child: const Text('HookWidget版'),
-        ),
-      ],
-    );
-  }
-}
