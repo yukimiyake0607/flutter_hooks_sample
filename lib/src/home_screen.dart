2e8bf92915476228c1b05b5b7725328ee3b5130f
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-19 21:29:22 +0900

    update: Memoを追加

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -23,6 +23,9 @@ class HomeScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 const MemorizedMenuButtons(),
+                const Text(
+                  'buildメソッドは何度も実行されるもの。キーボード表示、スクロール、setStateなど。buildメソッド内のフィルタリング計算はとても重くbuildメソッドごとに実行するとパフォーマンスが悪くなる。そこで前回テキストと比較してテキストが変更した時のみ計算をすればパフォーマンスが良くなる。しかし手動で実装するのは賢くない。useMemorizedを使用すれば変数を自分で管理する必要ない、比較ロジックも必要ないなどメリットがある。',
+                ),
               ],
             ),
           ),
