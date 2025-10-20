cc5b3234b44f9c92cedc099a5e9e01847511032f
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-20 18:46:18 +0900

    add: 追加忘れ

---
+++ lib/src/feature/text_editing/profile_edit_stateful.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileEditStateful extends StatefulWidget {
+  const ProfileEditStateful({super.key});
+
+  @override
+  State<ProfileEditStateful> createState() => _ProfileEditStatefulState();
+}
+
+class _ProfileEditStatefulState extends State<ProfileEditStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
