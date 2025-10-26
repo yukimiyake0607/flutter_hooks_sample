4c4076b542a3474585a934d6c9fe0cd6acef3b47
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-26 21:37:27 +0900

    add: initState

--- lib/src/feature/animation/animation_stateful.dart
+++ lib/src/feature/animation/animation_stateful.dart
@@ -7,11 +7,36 @@ class AnimationStateful extends StatefulWidget {
   State<AnimationStateful> createState() => _AnimationStatefulState();
 }
 
-class _AnimationStatefulState extends State<AnimationStateful> with SingleTickerProviderStateMixin {
-  
+class _AnimationStatefulState extends State<AnimationStateful>
+    with SingleTickerProviderStateMixin {
   // 手動でAnimationController作成
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // initStateで初期化
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: Center(child: Text('animation')));
   }
 }
