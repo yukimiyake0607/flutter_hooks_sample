7f2a2058cdda6e8b7a54c033b55cc85dfbc8d2eb
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-30 16:39:59 +0900

    update: scroll_stateful initialize

--- lib/src/feature/scroll/scroll_stateful.dart
+++ lib/src/feature/scroll/scroll_stateful.dart
@@ -8,6 +8,43 @@ class ScrollStateful extends StatefulWidget {
 }
 
 class _ScrollStatefulState extends State<ScrollStateful> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showBackToTop = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // リスナーを手動で登録
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.offset > 200 && !_showBackToTop) {
+      setState(() {
+        _showBackToTop = true;
+      });
+    } else if (_scrollController.offset <= 200 && _showBackToTop) {
+      setState(() {
+        _showBackToTop = false;
+      });
+    }
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
