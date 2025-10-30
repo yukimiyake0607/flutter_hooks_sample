e52e253504f6e441c1a9a4b844e8807aba971fc6
yukimiyake0607 <yukimiyake38@gmail.com>
2025-10-30 14:23:27 +0900

    update: focus_hooks

--- lib/src/feature/focus_node/focus_hooks.dart
+++ lib/src/feature/focus_node/focus_hooks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class FocusHooks extends HookConsumerWidget {
@@ -6,7 +7,57 @@ class FocusHooks extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    
-    return Container();
+    // 自動でdispose
+    final emailFocusNode = useFocusNode();
+    final passwordFocusNode = useFocusNode();
+
+    void moveToPassword() {
+      passwordFocusNode.requestFocus();
+    }
+
+    void moveToEmail() {
+      emailFocusNode.requestFocus();
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text('HookWidget版')),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              focusNode: emailFocusNode,
+              decoration: const InputDecoration(
+                labelText: 'メールアドレス',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              focusNode: passwordFocusNode,
+              decoration: const InputDecoration(
+                labelText: 'パスワード',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => moveToPassword(),
+              child: Text('パスワード欄へ移動'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => moveToEmail(),
+              child: Text('メールアドレス欄へ移動'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
